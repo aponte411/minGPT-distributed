f935094fce984b68bfcac08ec5ac961f759d59cf
aponte411 <aponted511@gmail.com>
2023-03-31 18:10:53 -0400

    feat: fixes

--- mingpt/model.py
+++ mingpt/model.py
@@ -59,6 +59,51 @@ class OptimizerConfig:
     betas: tuple = (0.9, 0.95)
 
 
+def create_optimizer(model: torch.nn.Module, optimizer_config: OptimizerConfig):
+    """
+    Separate params into those with and without weight decay.
+    Without decay: bias, layernorm, embeddings
+    """
+    decay = set()
+    no_decay = set()
+    whitelist_weight_modules = (torch.nn.Linear, )
+    blacklist_weight_modules = (torch.nn.LayerNorm, torch.nn.Embedding)
+    for mn, m in model.named_modules():
+        for pn, p in m.named_parameters():
+            fpn = '%s.%s' % (mn, pn) if mn else pn # full param name
+            # random note: because named_modules and named_parameters are recursive
+            # we will see the same tensors p many many times. but doing it this way
+            # allows us to know which parent module any tensor p belongs to...
+            if pn.endswith('bias'):
+                # all biases will not be decayed
+                no_decay.add(fpn)
+            elif pn.endswith('weight') and isinstance(m, whitelist_weight_modules):
+                # weights of whitelist modules will be weight decayed
+                decay.add(fpn)
+            elif pn.endswith('weight') and isinstance(m, blacklist_weight_modules):
+                # weights of blacklist modules will NOT be weight decayed
+                no_decay.add(fpn)
+
+    # validate that we considered every parameter
+    param_dict = {pn: p for pn, p in model.named_parameters()}
+    inter_params = decay & no_decay
+    union_params = decay | no_decay
+    assert len(inter_params) == 0, "parameters %s made it into both decay/no_decay sets!" % (str(inter_params), )
+    assert len(param_dict.keys() - union_params) == 0, "parameters %s were not separated into either decay/no_decay set!" \
+                                                % (str(param_dict.keys() - union_params), )
+
+    # create the pytorch optimizer object
+    optim_groups = [
+        {"params": [param_dict[pn] for pn in sorted(list(decay))], "weight_decay": optimizer_config.weight_decay},
+        {"params": [param_dict[pn] for pn in sorted(list(no_decay))], "weight_decay": 0.0},
+    ]
+    optimizer = torch.optim.AdamW(
+        optim_groups,
+        lr=optimizer_config.learning_rate,
+        betas=optimizer_config.betas,
+    )
+    return optimizer
+
 class MultiHeadSelfAttention(nn.Module):
     """
     Multi-Head Self-Attention with additional projection.
