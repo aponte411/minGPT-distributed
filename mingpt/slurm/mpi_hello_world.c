0d5fd140559cd0bdbbbc67a0f4f7e8e7fd73671e
aponte411 <aponted511@gmail.com>
2023-04-06 17:13:41 -0400

    chore: add helloworld to test hpc cluster

---
+++ mingpt/slurm/mpi_hello_world.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <unistd.h>
+
+int main(int argc, char **argv){
+	int step, node, hostlen;
+	char hostname[250];
+	hostlen = 255;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &node);
+	MPI_Get_processor_name(hostname, &hostlen);
+	for (step = 1; step < 5; step++){
+		printf("Hello from step %d on node %d,(%s)\n",
+                step, node, hostname);
+        sleep(2);
+    }
+    MPI_Finalize();
+}
+
